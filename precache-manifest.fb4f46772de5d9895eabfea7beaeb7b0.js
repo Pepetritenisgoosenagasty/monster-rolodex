b0ca1accc0495b67a7a5cf77a7d2b6895e806822
Pepetritenisgoosenagasty <adjalokoedward@gmail.com>
2020-08-26 17:14:12 +0000

    Updates

---
+++ precache-manifest.fb4f46772de5d9895eabfea7beaeb7b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b00d2150df07cc5870e849e809c5b764",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "2eaefc68715be217603a",
+    "url": "/monster-rolodex/static/css/main.b0c1d7a3.chunk.css"
+  },
+  {
+    "revision": "ef594503646c778d0fba",
+    "url": "/monster-rolodex/static/js/2.5cd5d515.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.5cd5d515.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2eaefc68715be217603a",
+    "url": "/monster-rolodex/static/js/main.ecf95910.chunk.js"
+  },
+  {
+    "revision": "d0e5931193defec3a6f0",
+    "url": "/monster-rolodex/static/js/runtime-main.cada054d.js"
+  }
+]);
